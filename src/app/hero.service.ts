128a3fd66ae1b8adbcd8c12b5ffc396ba17cfbf8
KonstKu <konstantin.kuts@web.de>
2022-04-08 12:07:19 +0200

    3 files modified

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -12,6 +12,9 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class HeroService {
   private heroesUrl = 'api/heroes';
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
   constructor(
     private http: HttpClient,
     private messageService: MessageService
@@ -27,6 +30,13 @@ export class HeroService {
     return this.http.get<Hero>(url).pipe(
       tap((_) => this.log(`fetched hero id=${id}`)),
       catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
+  }
+  /** PUT: update the hero on the server */
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap((_) => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
     );
   }
   /** Log a HeroService message with the MessageService */
